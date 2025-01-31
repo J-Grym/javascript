86de4f14959a53b21c2ae547bdac5cad67111f6b
Jorge dos Santos Rosa <monsenhor5@gmail.com>
2025-01-31 13:48:39 -0300

    quase pronto

--- PESSOAL/calculadora/calc.index/index.js
+++ PESSOAL/calculadora/calc.index/index.js
@@ -2,13 +2,27 @@ let display = document.getElementById('display')
 let sinal = false
 let lembrete = document.getElementById('lembrete')
 let memo = []
+let start = false
+let valido = false
 
 function insert(num){
-    display.textContent += num
-    sinal = false
-}
+    if (display.textContent == ''){
+        start = false
+        display.textContent = ''
+    }else if(display.textContent == 0){
+        start = false
+        display.textContent = num
+    } else {
+        start = true
+        sinal = false
+        display.textContent += num
+    }
+}      
 function operadores(oper){
-    if (sinal){
+    if (display.textContent == ''){
+        start = false
+        display.textContent = ''
+    } else if (sinal){
         display.textContent = oper
     } else {
         sinal = true
@@ -16,29 +30,38 @@ function operadores(oper){
         
     }
 }
-function apagar(){
+function doisClicks(){
     display.textContent = ''
     lembrete.style.backgroundColor = ''
+    lembrete.style.boxShadow = ''
+    memo = []
+}
+function reset(){
+    display.textContent = 0
 }
 function calcular(){
     let display = document.getElementById('display').textContent
     document.getElementById('display').textContent = eval(display)
-    display = Number(display.value)
 }
 function back(){
     let display = document.getElementById('display').textContent
     document.getElementById('display').textContent = display.substring(0, display.length -1)
+   
 }
 function limpar(){
     memo = []
     lembrete.style.backgroundColor = ''
+    lembrete.style.boxShadow = ''
 }
 function memoria(){
     let novoNumero = document.getElementById('display').textContent
     memo.push(novoNumero)
     lembrete.style.backgroundColor = 'green'
-    display.textContent = ''
+    lembrete.style.boxShadow = '0px 0px 15px red, inset 0px 0px 15px red'
+    display.textContent = 0
 }
 function retorne(){
-    display.textContent += memo.toString()
+    if (display.textContent == 0){
+        display.textContent = memo.toString()
+    }
 }
