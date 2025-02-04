1dab99216f09eefe07ff09205ea1e2c8fa2d27bd
Jorge dos Santos Rosa <monsenhor5@gmail.com>
2025-02-04 15:26:39 -0300

    melhorias

--- PESSOAL/calculadora/calc.index/index.js
+++ PESSOAL/calculadora/calc.index/index.js
@@ -1,82 +1,101 @@
 let display = document.getElementById('display')
+const numeros = document.querySelectorAll('[id*=clique]')
+let operando2 = document.getElementById('operando2')
 let lembrete = document.getElementById('lembrete')
-let gatilho 
-let numberAnterior
-let operar = []
+let gatilho
+let numeroAnterior
+let operador
 let localC
 
-function doisClicks(){
-    display.textContent = ''
-    localC = ''
-    lembrete.style.backgroundColor = ''
-    lembrete.style.boxShadow = ''
-    numberAnterior = ''
-}
-function reset(){
-        gatilho = false
-        display.textContent = 0
-}
-function insert(num){
-    if(display.textContent == ''){
+const insert = (num) => { 
+    if (display.textContent == ''){
         display.textContent = ''
-    } else if(gatilho == false && display.textContent == 0){
+    }else if(gatilho == false){
         display.textContent = num
-        gatilho = true  
+        gatilho = true
     }else if(gatilho == true){
         display.textContent += num
     }
 }
-function ponto(num){
-    if (display.textContent == ''){
+const memoria = () => {
+    if(display.textContent == ''){
         display.textContent = ''
-    } else if(gatilho == true || display.textContent == 0){
-        display.textContent += num
-        gatilho = true
-    }
-}
-function operadores(oper){
-        numberAnterior = display.textContent 
-        operar.push(oper)
-        display.textContent = 0
-        gatilho = false
-        operar2 = []
-}
-
-function calcular(){
-    if(display.textContent == 0){
-        display.textContent = 0
     } else {
-        let display = document.getElementById('display').textContent
-        document.getElementById('display').textContent = eval(numberAnterior + operar.toString() + display).toFixed(2)
-        gatilho = false
-        operar = []
-        numberAnterior = ''
-        controle = ''
-    }
-
-}
-function porcent(){
-    let display = document.getElementById('display').textContent
-    document.getElementById('display').textContent = eval(numberAnterior / 100 * display)
-}
-function back(){
-    let display = document.getElementById('display').textContent
-    document.getElementById('display').textContent = display.substring(0, display.length -1)
-}
-function memoria(){
     localC = display.textContent
+    display.textContent = 0
     lembrete.style.backgroundColor = 'green'
     lembrete.style.boxShadow = '0px 0px 5px green, inset 0px 0px 5px green'
-    display.textContent = 0
+    }
 }
-function retorne(){
+const retorne = () => {
+    if(display.textContent == ''){
+        display.textContent = ''
+    } else if(localC == undefined){
+        display.textContent = 0
+    } else {
     display.textContent = localC
+    }
 }
-function limpar(){
-    localC = 0
+const limpar = () => {
+    localC = undefined
     lembrete.style.backgroundColor = ''
     lembrete.style.boxShadow = ''
 }
 
+const ponto = (p) => {
+    if(display.textContent == ''){
+        display.textContent = ''
+    } else {
+    display.textContent += p
+    gatilho = true
+    }
+}
+const operadores = (oper) => {
+    if(display.textContent == ''){
+        display.textContent = ''
+    } else if(numeroAnterior == undefined){
+        numeroAnterior = display.textContent
+        operador = oper
+        operando2.textContent = oper
+        gatilho = false
 
+    }else{
+        display.textContent = parseFloat(eval(`${numeroAnterior}${operador}${display.textContent}`))
+        numeroAnterior = display.textContent
+        operador = oper
+        operando2.textContent = oper
+        gatilho = false
+    }
+}
+const porcent = () => display.textContent = parseFloat(eval(`${numeroAnterior} / 100 * ${display.textContent}`))
 
+const calcular = () => {
+    display.textContent = parseFloat(eval(`${numeroAnterior}${operador}${display.textContent}`))
+    numeroAnterior = ''
+    operador = ''
+    operando2.textContent = ''
+    gatilho = false
+}
+const back = () => {
+    if(display.textContent == ''){
+        display.textContent = ''
+    } else {
+    display = document.getElementById('display').textContent
+    document.getElementById('display').textContent = display.substring(0, display.length -1)
+    }
+}
+const reset = () => {
+    display.textContent = 0
+    operador = ''
+    operando2.textContent = ''
+    gatilho = false
+}
+const doisClicks = () => {
+    localC = ''
+    lembrete = ''
+    display.textContent = ''
+    operador = ''
+    operando2.textContent = ''
+    gatilho = ''
+    window.location.reload()
+}
